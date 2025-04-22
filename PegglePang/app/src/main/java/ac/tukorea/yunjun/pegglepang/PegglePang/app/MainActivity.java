09aadb98ff20d86d61d737c40e7e16191de039c5
YJ0717 <cafricool3@naver.com>
2025-04-22 20:07:49 +0900

    리소스 추가 및 메인화면 텍스트 및 버튼투명적용

--- PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/app/MainActivity.java
+++ PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/app/MainActivity.java
@@ -3,6 +3,7 @@ package ac.tukorea.yunjun.pegglepang.PegglePang.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import ac.tukorea.yunjun.pegglepang.R;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 시작 화면에 이미지 설정
+        ImageView startScreenImage = findViewById(R.id.start_screen_image);
+        startScreenImage.setImageResource(R.mipmap.startscreen); // mipmap 폴더에서 이미지 설정
     }
 
     public void onBtnStartGame(View view) {
