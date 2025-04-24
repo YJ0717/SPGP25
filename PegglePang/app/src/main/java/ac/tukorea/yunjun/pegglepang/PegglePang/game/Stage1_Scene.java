04046093d9ac3ccd8c587f6f3b847974de86ecbb
YJ0717 <cafricool3@naver.com>
2025-04-24 11:27:32 +0900

    xml이미지 뷰를 이용해여 스테이지1선택창 배경설정 및 뒤로가기 텍스트 생성

--- PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/game/Stage1_Scene.java
+++ PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/game/Stage1_Scene.java
@@ -5,7 +5,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.content.Context;
 import android.view.MotionEvent;
+import android.app.Activity;
+import android.widget.TextView;
 
+import ac.tukorea.yunjun.pegglepang.R;
 import ac.tukorea.yunjun.pegglepang.framework.scene.Scene;
 
 public class Stage1_Scene extends Scene {
@@ -15,29 +18,23 @@ public class Stage1_Scene extends Scene {
     public Stage1_Scene(Context context) {
         this.context = context;
         paint = new Paint();
-        paint.setColor(Color.WHITE); 
+        paint.setColor(Color.WHITE);
+        
+        Activity activity = (Activity) context;
+        activity.setContentView(R.layout.stage1_select);
+        
+        TextView backText = activity.findViewById(R.id.back_text);
+        backText.setOnClickListener(v -> Scene.pop());
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        canvas.drawColor(Color.BLACK); 
-        paint.setTextSize(50);
-        canvas.drawText("뒤로가기", 400, 400, paint); 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            float x = event.getX();
-            float y = event.getY();
-            // 터치 이벤트 처리
-            if (x > 100 && x < 300 && y > 300 && y < 400) { // 특정 영역을 클릭했을 때
-                Scene.pop(); // 이전 씬으로 돌아가기
-                return true;
-            }
-        }
-        return false; // 이벤트가 처리되지 않음
+        return false;
     }
 
     @Override
