fa375cc6ff3ffbbeaff54de035648ef3bbe72077
YJ0717 <cafricool3@naver.com>
2025-05-01 18:49:55 +0900

    스테이지1-1씬의 영역 재설정

--- PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/game/S1_1.java
+++ PegglePang/app/src/main/java/ac/tukorea/yunjun/pegglepang/PegglePang/game/S1_1.java
@@ -1,5 +1,5 @@
-// 스테이지 1-1의 구현. 게임 화면을 3개 영역(전투, 플레이어 정보, 퍼즐)으로 나누어 표시
-// 각 영역은 화면 높이의 1/3씩 차지
+// 스테이지 1-1의 구현. 게임 화면을 3개 영역으로 나누어 표시
+// 퍼즐(55%, 하단), 전투(30%, 상단), 플레이어 정보(15%, 중단) 영역의 비율로 구성
 
 package ac.tukorea.yunjun.pegglepang.PegglePang.game;
 
@@ -28,19 +28,22 @@ public class S1_1 extends BaseStageScene {
     public void draw(Canvas canvas) {
         canvas.drawColor(Color.LTGRAY);
 
-        float firstDivider = Metrics.height / 3f;
-        float secondDivider = 2 * Metrics.height / 3f;
+        // 화면을 55:15:30 비율로 나눔 -> 하단부터 퍼즐:정보:전투
+        float puzzleStart = Metrics.height * 0.45f;     
+        float playerInfoStart = Metrics.height * 0.30f;  
+                                                        
 
-        canvas.drawLine(0, firstDivider, Metrics.width, firstDivider, linePaint);
-        canvas.drawLine(0, secondDivider, Metrics.width, secondDivider, linePaint);
+        canvas.drawLine(0, puzzleStart, Metrics.width, puzzleStart, linePaint);
+        canvas.drawLine(0, playerInfoStart, Metrics.width, playerInfoStart, linePaint);
 
         Paint textPaint = new Paint();
         textPaint.setColor(Color.BLACK);
         textPaint.setTextSize(50);
         textPaint.setTextAlign(Paint.Align.CENTER);
 
-        canvas.drawText("전투 공간", Metrics.width/2, firstDivider/2, textPaint);
-        canvas.drawText("플레이어 정보", Metrics.width/2, (firstDivider + secondDivider)/2, textPaint);
-        canvas.drawText("퍼즐 영역", Metrics.width/2, (secondDivider + Metrics.height)/2, textPaint);
+        // 각 영역의 중앙에 텍스트 배치
+        canvas.drawText("전투 공간", Metrics.width/2, playerInfoStart/2, textPaint);
+        canvas.drawText("플레이어 정보", Metrics.width/2, playerInfoStart + (puzzleStart - playerInfoStart)/2, textPaint);
+        canvas.drawText("퍼즐 영역", Metrics.width/2, puzzleStart + (Metrics.height - puzzleStart)/2, textPaint);
     }
 }
